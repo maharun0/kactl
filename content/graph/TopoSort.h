d90fc26193154ef6c6fadcae13d73aa11922ab42
maharun0 <coderbeast7@gmail.com>
2024-12-05 02:59:51 +0000

    added my topsort template too

--- content/graph/TopoSort.h
+++ content/graph/TopoSort.h
@@ -19,3 +19,22 @@ vi topoSort(const vector<vi>& gr) {
 		if (--indeg[x] == 0) q.push_back(x);
 	return q;
 }
+// Method 2 (my method)
+// Topological sort in DAG (using DFS)
+#define clear_graph(n) for(int i=0;i<=n;i++){g[i].clear(),vis[i]=false;} path.clear(); nodes = set<int>();
+const int N = 1e5+10;
+vector<int> g[N];
+bool vis[N];
+
+deque<int> path; // path in assending order
+set<int> nodes; // all distinct nodes
+
+void dfs(int vertex){
+	vis[vertex] = true;
+	for (int child : g[vertex]) if(!vis[child]) dfs(child);
+	path.push_front(vertex);
+}
+// run dfs from each node
+void TopSort() { for (auto& i : nodes) if (!vis[i]) dfs(i); }
+// print(path);
+
